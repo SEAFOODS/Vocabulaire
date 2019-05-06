e339cdb43cbcb22b59c949ccefcc4332fceb8186
SEAFOODS <43834030+SEAFOODS@users.noreply.github.com>
2019-05-06 13:51:29 -0700

    added new random question picker method
    will finish later

--- api.js
+++ api.js
@@ -50,3 +50,89 @@ export function deleteWord(id) {
         method: 'DELETE',
     })
 }
+export function writeQuestion(type) {
+    if(type == 1) {
+        return (
+            <View style={{ flex: 1 }}>
+                <View key={this.state.word.id} style={styles.questionCard}>
+                    <View style={styles.vocabContainer}>
+                        <View style={styles.vocab}>
+                            <Text style={styles.word}>What is the id of {this.state.length}?</Text>
+                        </View>
+                    </View>
+                </View>
+                <View key={this.state.word.id} style={styles.answerCard}>
+                    <View style={styles.vocabContainer}>
+                        <View style={styles.vocab}>
+                             <TouchableHighlight>
+                                <Text style={styles.word}>{this.state.word.id}</Text>
+                             </TouchableHighlight>
+                        </View>
+                    </View>
+                </View>
+             </View>)
+    }
+    if(type == 2) {
+        return (
+            <View style={{ flex: 1 }}>
+                <View key={this.state.word.id} style={styles.questionCard}>
+                    <View style={styles.vocabContainer}>
+                        <View style={styles.vocab}>
+                            <Text style={styles.word}>What is the definition of {this.state.length}?</Text>
+                        </View>
+                    </View>
+                </View>
+                <View key={this.state.word.id} style={styles.answerCard}>
+                    <View style={styles.vocabContainer}>
+                        <View style={styles.vocab}>
+                             <TouchableHighlight>
+                                <Text style={styles.word}>{this.state.word.definition}</Text>
+                             </TouchableHighlight>
+                        </View>
+                    </View>
+                </View>
+             </View>)
+    }
+    if(type == 3) {
+        return (
+            <View style={{ flex: 1 }}>
+                <View key={this.state.word.id} style={styles.questionCard}>
+                    <View style={styles.vocabContainer}>
+                        <View style={styles.vocab}>
+                            <Text style={styles.word}>What part of speech is this word{this.state.length}?</Text>
+                        </View>
+                    </View>
+                </View>
+                <View key={this.state.word.id} style={styles.answerCard}>
+                    <View style={styles.vocabContainer}>
+                        <View style={styles.vocab}>
+                             <TouchableHighlight>
+                                <Text style={styles.word}>{this.state.word.partofspeech}</Text>
+                             </TouchableHighlight>
+                        </View>
+                    </View>
+                </View>
+             </View>)
+    }
+    if(type == 4) {
+        return (
+            <View style={{ flex: 1 }}>
+                <View key={this.state.word.id} style={styles.questionCard}>
+                    <View style={styles.vocabContainer}>
+                        <View style={styles.vocab}>
+                            <Text style={styles.word}>What word fits this definition{this.state.length}?</Text>
+                        </View>
+                    </View>
+                </View>
+                <View key={this.state.word.id} style={styles.answerCard}>
+                    <View style={styles.vocabContainer}>
+                        <View style={styles.vocab}>
+                             <TouchableHighlight>
+                                <Text style={styles.word}>{this.state.word.word}</Text>
+                             </TouchableHighlight>
+                        </View>
+                    </View>
+                </View>
+             </View>)
+    }
+}
